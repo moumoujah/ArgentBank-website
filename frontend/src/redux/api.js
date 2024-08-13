1c798e647fa6d7ddec17532459133f5dd5f3de0b
Bastien MOULIN <bastien.moulin@hilit.fr>
2024-08-13 23:51:33 +0200

    Feat: add api.js for authentification

---
+++ frontend/src/redux/api.js
@@ -0,0 +1,20 @@
+const apiUrl = "http://localhost:3001/api/v1/";
+
+// Authentification de L'email ou le mot de passe dans le formulaire 
+export const fetchLoginUser = async (email, password) => {
+    try {
+        const response = await fetch(`${apiUrl}user/login`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password })
+        })
+        if (response.status === 400) {    //Alerte si une erreur de saisie email ou mot de passe
+            alert("Mail ou mot de passe invalide");
+        } 
+        const data = await response.json();
+        return data;
+    } 
+    catch (err) {
+        console.error(err);
+    };
+}
