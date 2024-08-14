fccf92d9e38653fff32ea3d2b59ab1f5bbbc86f8
Bastien MOULIN <bastien.moulin@hilit.fr>
2024-08-14 18:46:47 +0200

    Feat: add setLogOut if user connected

--- frontend/src/layout/header/Header.jsx
+++ frontend/src/layout/header/Header.jsx
@@ -1,7 +1,19 @@
 import LogoArgentBank from "./argentBankLogo.webp";
 import { NavLink } from 'react-router-dom';
+import { setLogOut } from "../../redux/reducers/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function Header() {
+
+  const token = useSelector((state) => state.auth.token);
+  const userName = useSelector((state) => state.user.userName);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(setLogOut());
+  };
+
+
     return (
     <header>
          <nav className="main-nav">
@@ -15,12 +27,30 @@ function Header() {
         <h1 className="sr-only">Argent Bank</h1>
       </div>
       </NavLink>
-      <NavLink to ='/sign-in' > 
-        <div className="main-nav-item">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </div>
-        </NavLink>
+      {token ? (  <div className="nav-signout">
+                   <NavLink to ='/user' > 
+                   <div className="main-nav-item">
+                     <i className="fa fa-user-circle"></i><div>{userName}</div>
+                     
+                   </div>
+                   </NavLink>
+                   <NavLink to ='/' onClick={handleLogout}> 
+                   <div className="main-nav-item">
+                     <i className="fa fa-sign-out"></i>
+                     Sign Out
+                   </div>
+                   </NavLink>
+                   </div>
+                   
+                ) : (
+                  <NavLink to ='/sign-in' > 
+                  <div className="main-nav-item">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                  </div>
+                  </NavLink>
+                )}
+      
     </nav>
    
     </header>)
