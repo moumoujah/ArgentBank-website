34118472168f04b98b2118eae5fbf571ec9b0b01
Bastien MOULIN <bastien.moulin@hilit.fr>
2024-08-14 18:27:47 +0200

    Feat: connexion ok and recording token in state

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -1,18 +1,51 @@
 import Button from "../button/Button.jsx";
 import InputWrapper from "../inputWrapper/InputWrapper.jsx";
+import { fetchLoginUser } from "../../redux/api.js";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setLogIn } from "../../redux/reducers/authSlice.js";
+import { useNavigate } from "react-router-dom";
+// import { NavLink } from 'react-router-dom';
 
-function Form() {
+function Form() { 
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+
+  // Soumission du formulaire
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fetchDataLogin = await fetchLoginUser(email, password);
+
+    // Enregistrement du token dans le state si l'email et le mot de passe sont correct
+    if (fetchDataLogin.status === 200) { 
+        const token = fetchDataLogin.body.token;
+        dispatch(setLogIn({ token }))
+        // Une fois la connexion réussi redirection ver la page user
+        navigate("/user") 
+    }
+  };
+
+
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
         <InputWrapper
                 id="username"
                 label="Username"
                 type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
             <InputWrapper
                 id="password"
                 label="Password"
                 type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
             />
         <div className="input-remember">
           <input type="checkbox" id="remember-me" /><label for="remember-me">
